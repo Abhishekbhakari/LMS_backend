85034bc8c907795d33dc876d2c44097ba8bb5c45
AbhishekBhakari <bhakariabhi@gmail.com>
2024-03-16 12:22:41 +0530

    Pushed Backed code

---
+++ app.js
@@ -0,0 +1,59 @@
+import cookieParser from 'cookie-parser';
+
+import express from 'express';
+import { config  } from 'dotenv';
+config();
+import cors from 'cors';
+import morgan from 'morgan';
+import errorMiddleware from './middlewares/error.middleware.js';
+
+
+const app = express();
+
+// Middlewares
+// Built-In
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Third-Party
+app.use(
+  cors({
+    origin:[process.env.FRONTEND_URL],
+    credentials: true,
+  })
+);
+app.use(morgan('dev'));
+app.use(cookieParser());
+
+// Server Status Check Route
+app.get('/ping', (_req, res) => {
+  res.send('Pong');
+});
+
+
+// Import all routes
+import userRoutes from './routes/user.routes.js';
+import courseRoutes from './routes/course.routes.js';
+import paymentRoutes from './routes/payment.routes.js';
+import miscRoutes from './routes/miscellaneous.routes.js';
+
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/courses', courseRoutes);
+app.use('/api/v1/payments', paymentRoutes);
+app.use('/api/v1', miscRoutes);
+
+// Additional CORS headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', true);
+  // Add other headers as needed
+  next();
+});
+
+// Default catch all route - 404
+app.all('*', (_req, res) => {
+  res.status(404).send('OOPS!!! 404 Page Not Found');
+});
+
+// Custom error handling middleware
+app.use(errorMiddleware);
+
+export default app;
