6dcbcba15c9ac5959004b4552b4c1d6e85255a71
Abhishek Bhakari <130546310+Abhishekbhakari@users.noreply.github.com>
2024-04-22 16:40:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 // Third-Party
 app.use(
   cors({
-    origin:[process.env.FRONTEND_URL],
+    origin:'https://lms-frontend-dusky-delta.vercel.app/',
     credentials: true,
   })
 );
