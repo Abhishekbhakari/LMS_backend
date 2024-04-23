6661c815cac1cc1e7692aee4d50f59c9a76aaecc
Abhishek Bhakari <130546310+Abhishekbhakari@users.noreply.github.com>
2024-04-23 12:47:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 // Third-Party
 app.use(
   cors({
-    origin: 'https://lms-frontend-git-main-abhisheks-projects-3276e3cb.vercel.app', 
+    origin: '*', 
     credentials: true,
   })
 );
